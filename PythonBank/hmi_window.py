fdfa056e83f0b44840305c67c8536a0f6ce3dd68
HugoMGSung <personar95@naver.com>
2021-08-23 20:02:45 +0900

    Pyqt mqtt 커밋

--- PythonBank/hmi_window.py
+++ PythonBank/hmi_window.py
@@ -4,7 +4,27 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
+
+import paho.mqtt.client as mqtt
+import json
+import datetime as dt
+import RPi.GPIO as GPIO
+import time
 # 여기에 GPIO 작업 및 MQTT 송수신을 처리함
+
+dev_id = "machine01"
+broker_address = "192.168.200.102"
+broker_port = 1883
+client = None
+
+redPin = 18
+greenPin = 23
+bluePin = 24
+
+GPIO.setmode(GPIO.BCM) # BOARD or BCM
+GPIO.setup(redPin, GPIO.OUT)
+GPIO.setup(greenPin, GPIO.OUT)
+GPIO.setup(bluePin, GPIO.OUT)
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -31,12 +51,29 @@ class MyWindow(QMainWindow):
         self.move(qr.topLeft())
 
     def btnOn_Clicked(self):
+        GPIO.output(greenPin, True)
+        GPIO.output(bluePin, False)
+        GPIO.output(redPin, False)
+
         print('LED를 켰습니다!!')
         self.lblStatus.setText('LED를 켰습니다')
+        global client
+        client.publish("machine01/4001/", json.dumps({"test": "pushpush"}), 1)
 
     def btnWarn_Clicked(self):
+        GPIO.output(greenPin, False)
+        GPIO.output(bluePin, False)
+        GPIO.output(redPin, True)
+
         print('경고상태.')
         self.lblStatus.setText('경고상태 입니다')
+        global client
+        client.publish("machine01/4001/", json.dumps({"test": "warnwarn"}), 1)
+
+
+client = mqtt.Client(dev_id)
+client.connect(broker_address, port=broker_port)
+client.loop()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
